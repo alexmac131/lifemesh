9f8eba8f9556a1a5affa8a995d7bb10d7c4347aa
Fred Walter <fred_walter@hotmail.com>
2018-10-20 17:46:06 -0400

    work-in-progress

--- arduino/everything/everything.ino
+++ arduino/everything/everything.ino
@@ -20,9 +20,11 @@ char longitude[] = "-80.4963671";
 char wifi_ssid[] = "Windows Phone7348";
 char wifi_pass[] = "E069$3y3";
 
+char server[] = "api.lifemesh.io";
+
 void setup()
 {
-  Serial.begin(9600); // Initialize serial and wait for port to open:
+  Serial.begin(9600); // Initialize serial and wait for port to open
   while (!Serial);    // Wait for serial port to connect. Needed for Leonardo only!
 
   wifi_setup();
@@ -30,17 +32,21 @@ void setup()
   // MQ has no setup() requirement
 }
 
-WiFiServer wifi_server(23);
-long wifi_last_RSSI = 1.1;
-boolean alreadyConnected = false; // whether or not the client was connected previously
+WiFiClient wifi_client;
 
 void wifi_setup()
 {
+  // Check if the shield is present
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    while (true); // don't continue
+  }
+
   // Disable SD card
   pinMode(4, OUTPUT);
   digitalWrite(4, HIGH);
 
-  // Attempt to connect to Wifi network:
+  // Attempt to connect to Wifi network
   int tryCount = 1;
   int wifi_status = WL_IDLE_STATUS;
   while (wifi_status != WL_CONNECTED) {
@@ -48,16 +54,20 @@ void wifi_setup()
     Serial.print(tryCount++);
     Serial.print(") to connect to WPA SSID: ");
     Serial.println(wifi_ssid);
-    // Connect to WPA/WPA2 network:
+    // Connect to WPA/WPA2 network
     wifi_status = WiFi.begin(wifi_ssid, wifi_pass);
 
-    delay(10000); // wait 10 seconds for connection:
+    delay(5000); // wait 5 seconds for connection
   }
-  wifi_server.begin();
-
-  // you're connected now, so print out the data:
   Serial.println("You're connected to the network!");
   wifi_println();
+
+  Serial.print("Starting connection to: ");
+  Serial.println(server);
+  if (wifi_client.connect(server, 80)) {
+    Serial.print("Connected to: ");
+    Serial.println(server);
+  }
 }
 
 // Connect VCC of the BMP085 sensor to 3.3V
@@ -80,27 +90,13 @@ void loop()
   Serial.print("\nloop_counter=");
   Serial.println(loop_counter++);
 
-  long wifi_RSSI = WiFi.RSSI(); // wait for a new client:
-  long delta = wifi_last_RSSI - wifi_RSSI;
-  wifi_last_RSSI = wifi_RSSI;
+  wifi_read();
 
-  WiFiClient client = wifi_server.available();
-  // when the client sends the first byte, say hello:
-  if (client) {
-    if (!alreadyConnected) {
-      // clear out the input buffer:
-      client.flush();
-      Serial.println("We have a new client!");
-      wifi_println();
-      client.println("Hello, client!");
-      alreadyConnected = true;
-    }
-
-    if (client.available() > 0) {
-      char thisChar = client.read(); // read the bytes incoming from the client:
-      wifi_server.write(thisChar);        // echo the bytes back to the client:
-      Serial.write(thisChar);        // echo the bytes to the wifi_server as well:
-    }
+  // If the server is disconnected then stop the client.
+  if (!wifi_client.connected()) {
+    Serial.println("\nDisconnected from server!");
+    wifi_client.stop();
+    while (true); // don't continue
   }
 
   bmp085_println();
@@ -120,6 +116,35 @@ void trim_leading_spaces(char *str)
   } else {
       *str = *p;
   }
+}
+
+void wifi_read()
+{
+  if (wifi_client.available()) {
+    while (wifi_client.available()) {
+      char c = wifi_client.read();
+//      Serial.write(c);
+    }
+//    Serial.println();
+  }
+}
+
+void wifi_post(char *content)
+{
+  wifi_client.println("POST /collector HTTP/1.1");
+  wifi_client.println("Host: api.lifemesh.io:80");
+  wifi_client.println("Accept: */*");
+  wifi_client.print("Content-Length: ");
+  wifi_client.println(strlen(content));
+  wifi_client.println("Connection: keep-alive");
+  wifi_client.println("Content-Type: application/json");
+  wifi_client.println();
+  wifi_client.println(content);
+
+  Serial.print("POSTed: ");
+  Serial.println(content);
+
+  wifi_read(); // display response to POST
 }
 
 void wifi_println()
@@ -147,15 +172,15 @@ void bmp085_println()
   dtostrf(bmp085.readTemperature(), 6, 2, buf_float);
   trim_leading_spaces(buf_float);
   sprintf(buf_main, "{\"latitude\":\"%s\",\"longitude\":\"%s\",\"temperature\":\"%s C\"}", latitude, longitude, buf_float);
-  Serial.println(buf_main);
+  wifi_post(buf_main);
 
   sprintf(buf_main, "{\"latitude\":\"%s\",\"longitude\":\"%s\",\"pressure\":\"%d Pa\"}", latitude, longitude, bmp085.readPressure());
-  Serial.println(buf_main);
+  wifi_post(buf_main);
 
   dtostrf(bmp085.readAltitude(), 8, 2, buf_float);
   trim_leading_spaces(buf_float);
   sprintf(buf_main, "{\"latitude\":\"%s\",\"longitude\":\"%s\",\"altitude\":\"%s m\"}", latitude, longitude, buf_float);
-  Serial.println(buf_main);
+  wifi_post(buf_main);
 }
 
 void mq_println()
@@ -164,5 +189,5 @@ void mq_println()
 
   int value = analogRead(0); // read analog input pin 0
   sprintf(buf_main, "{\"latitude\":\"%s\",\"longitude\":\"%s\",\"air_quality\":\"%d PPM\"}", latitude, longitude, value);
-  Serial.println(buf_main);
+  wifi_post(buf_main);
 }
